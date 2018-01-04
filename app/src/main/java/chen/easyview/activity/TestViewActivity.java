7a00c40075a579e32d0aa582ffd51449c76b0242
chennuo <617322616@qq.com>
2018-01-04 18:02:28 +0800

    测试悬浮窗

--- app/src/main/java/chen/easyview/activity/TestViewActivity.java
+++ app/src/main/java/chen/easyview/activity/TestViewActivity.java
@@ -1,15 +1,85 @@
 package chen.easyview.activity;
 
+import android.content.Context;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import chen.easyview.R;
+import chen.easyview.base.BaseApplication;
+import chen.easyview.view.MyFloatView;
 
 public class TestViewActivity extends AppCompatActivity {
+
+    MyFloatView myFV;
+    private WindowManager wm = null;
+    private WindowManager.LayoutParams wmParams = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_view);
+        createView();
+    }
+
+    private void createView() {
+
+        myFV = new MyFloatView(getApplicationContext());
+        myFV.setImageResource(R.drawable.im_select);  //这里简单的用自带的Icom来做演示
+        myFV.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+        //获取WindowManager
+
+        wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        //设置LayoutParams(全局变量）相关参数
+        wmParams = ((BaseApplication) getApplication()).getMywmParams();
+
+        /**
+         *以下都是WindowManager.LayoutParams的相关属性
+         * 具体用途可参考SDK文档
+         */
+
+        wmParams.type = WindowManager.LayoutParams.TYPE_PHONE;   //设置window type
+
+        wmParams.format = PixelFormat.RGBA_8888;   //设置图片格式，效果为背景透明
+
+        //设置Window flag
+
+        wmParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+
+        /**
+         * 下面的flags属性的效果形同“锁定”。
+         * 悬浮窗不可触摸，不接受任何事件,同时不影响后面的事件响应。
+         wmParams.flags=LayoutParams.FLAG_NOT_TOUCH_MODAL
+
+         | LayoutParams.FLAG_NOT_FOCUSABLE
+
+         | LayoutParams.FLAG_NOT_TOUCHABLE;
+         */
+
+        wmParams.gravity = Gravity.LEFT | Gravity.TOP;   //调整悬浮窗口至左上角，便于调整坐标
+
+        //以屏幕左上角为原点，设置x、y初始值
+        wmParams.x = 0;
+        wmParams.y = 0;
+        //设置悬浮窗口长宽数据
+        wmParams.width = 100;
+        wmParams.height = 100;
+        //显示myFloatView图像
+        wm.addView(myFV, wmParams);
+    }
+
+    @Override
+
+    public void onDestroy() {
+
+        super.onDestroy();
+
+        //在程序退出(Activity销毁）时销毁悬浮窗口
+
+        wm.removeView(myFV);
+
     }
 }
--- app/src/main/java/chen/easyview/base/BaseApplication.java
+++ app/src/main/java/chen/easyview/base/BaseApplication.java
@@ -2,6 +2,7 @@ package chen.easyview.base;
 
 import android.app.Application;
 import android.content.Context;
+import android.view.WindowManager;
 
 import com.socks.library.KLog;
 import com.squareup.leakcanary.LeakCanary;
@@ -70,4 +71,16 @@ public class BaseApplication extends Application {
         return daoSession;
     }
 
+    /**
+     * 创建全局变量
+     * 全局变量一般都比较倾向于创建一个单独的数据类文件，并使用static静态变量
+     *
+     * 这里使用了在Application中添加数据的方法实现全局变量
+     * 注意在AndroidManifest.xml中的Application节点添加android:name=".MyApplication"属性
+     *
+     */
+    private WindowManager.LayoutParams wmParams=new WindowManager.LayoutParams();
+    public WindowManager.LayoutParams getMywmParams(){
+        return wmParams;
+    }
 }
